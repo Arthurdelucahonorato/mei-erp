735a18f123b3f4cc57f366c4fd0468e1842878c9
KenielDev <kenielalves3@gmail.com>
2023-09-25 17:16:08 -0300

    REFACTOR: alterado tipo de dado randomico do faker

--- src/components/Charts/Line.tsx
+++ src/components/Charts/Line.tsx
@@ -8,6 +8,7 @@ import {
   Title,
   Tooltip,
   Legend,
+  ChartOptions,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { faker } from "@faker-js/faker";
@@ -24,6 +25,7 @@ ChartJS.register(
 
 export const options = {
   responsive: true,
+
   plugins: {
     legend: {
       position: "top" as const,
@@ -42,13 +44,13 @@ export const data = {
   datasets: [
     {
       label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
       label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
