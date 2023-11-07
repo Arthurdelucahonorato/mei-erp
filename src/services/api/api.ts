f8d416c6f885d44b5e86f18aa4afdcd803023cba
KenielDev <kenielalves3@gmail.com>
2023-11-07 19:35:22 -0300

    Update api.ts

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { parseCookies } from "nookies";
 
-const API_URL = "http://localhost:3000"; // Desenvolvimento com o back rodando local
-// const API_URL = "https://inoven-api.onrender.com"; // Produção
+//const API_URL = "http://localhost:3000"; // Desenvolvimento com o back rodando local
+const API_URL = "https://inoven-api.onrender.com"; // Produção
 
 // Instância de api
 export const api = axios.create({
