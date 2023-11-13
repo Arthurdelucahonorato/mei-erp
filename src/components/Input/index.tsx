335ca7072f4c5f16f33dbe069f541f4955855f03
KenielDev <kenielalves3@gmail.com>
2023-11-13 11:16:04 -0300

    REFACTOR: alterações de tipagem e estilização

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef, ComponentProps } from "react";
-import Lov from '@/components/Lov'
+import Lov from "@/components/Lov";
 
 type InputProps = {
   label?: string;
@@ -7,12 +7,24 @@ type InputProps = {
   errorMessage?: string;
   required?: boolean;
   lovButton?: LovType;
+  containerClassName: string;
 } & ComponentProps<"input">;
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ htmlFor, label, errorMessage, className, required, lovButton, ...props }, ref) => {
+  (
+    {
+      htmlFor,
+      label,
+      errorMessage,
+      containerClassName,
+      required,
+      lovButton,
+      ...props
+    },
+    ref
+  ) => {
     return (
-      <div className={`flex flex-col gap-2 ${className}`}>
+      <div className={`flex flex-col gap-2 ${containerClassName}`}>
         <div className="flex flex-row">
           <label
             htmlFor={htmlFor}
@@ -29,11 +41,18 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
             {...props}
             ref={ref}
             id={htmlFor}
-            className={`text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 dark:text-white bg-gray-200 dark:bg-theme-dark.100 leading-tight focus:outline-none focus:shadow-outline h-10 ${errorMessage && " border-[1px] border-red-500"}`}
+            className={`text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 dark:text-white bg-gray-200 dark:bg-theme-dark.100 leading-tight focus:outline-none focus:shadow-outline h-10 ${
+              errorMessage && " border-[1px] border-red-500"
+            }`}
           />
-          {lovButton &&
-            <Lov title={lovButton.title} listLabels={lovButton.listLabels} listValues={lovButton.listValues} onClick={lovButton.onClick} />
-          }
+          {lovButton && (
+            <Lov
+              title={lovButton.title}
+              listLabels={lovButton.listLabels}
+              listValues={lovButton.listValues}
+              onClick={lovButton.onClick}
+            />
+          )}
         </div>
 
         {errorMessage && (
